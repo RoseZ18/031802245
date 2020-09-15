a381dc1ac3e1f27dc4f4ba0a2029189836e1557c
RoseZ <2283156822@qq.com>
2020-09-16 01:54:23 +0800

    test commit

---
+++ src/com/RoseZ/Main.java
@@ -0,0 +1,13 @@
+package com.RoseZ;
+
+import java.text.DecimalFormat;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Inputpath.processpath(args[0],args[1],args[2]);
+            //Inputpath.processpath("tests/orig.txt","tests/orig_0.8_add.txt","ans.txt");
+            System.exit(0);
+        }
+
+}
---
+++ src/com/RoseZ/Txtprocess.java
@@ -0,0 +1,57 @@
+package com.RoseZ;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+public class Txtprocess {
+    private static final String html_seperator="<br>";
+    private static final String sentence_seperator="[,.;!:，。；!：]";
+    public static String[] split(String src)
+    {
+        List<String> result=new ArrayList<>();
+        String []htmls=src.split(html_seperator);
+        for (String s:htmls)
+            for (String s2:s.split(sentence_seperator))
+                result.add(s2);
+        String []results=new String[result.size()];
+        result.toArray(results);
+        return results;
+    }
+    //清晰字符串除杂消除干扰
+     static String[] clean(String []src)
+    {
+        for (int i=0;i<src.length;i++)
+            src[i]=clean(src[i]);
+        return src;
+    }
+    //清除非数字、字母、中文
+    public static String clean(String src)
+    {
+        return src.replaceAll("[^a-zA-Z0-9\\u4e00-\\u9fa5]","");
+    }
+    public static String ReadtoString(String filepath){
+        Reader reader = null;
+        BufferedReader br = null;
+        String data = null;
+        StringBuffer sb = new StringBuffer();
+        try {
+            reader = new FileReader(filepath);
+            br = new BufferedReader(reader);
+            while ((data = br.readLine()) != null) {
+                sb.append(data);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+                br.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+}
